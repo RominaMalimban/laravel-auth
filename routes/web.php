2ed4bdf3a0ee3789dab64a8c9892d26bba7d654a
RominaMalimban <romina.lyn@hotmail.it>
2023-02-08 17:12:31 +0100

    creazione funzione home e collegamento rotta

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\MainController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,9 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [MainController::class,'home'])
+    ->name('home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
