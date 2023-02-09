9894836fd582d5ae5194d7e2c43f11270d1379c9
RominaMalimban <romina.lyn@hotmail.it>
2023-02-09 11:07:41 +0100

    completamento seeder

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use App\Models\Project;
 class ProjectSeeder extends Seeder
 {
     /**
@@ -14,6 +14,6 @@ class ProjectSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Project::factory()->count(60)->create();
     }
 }
