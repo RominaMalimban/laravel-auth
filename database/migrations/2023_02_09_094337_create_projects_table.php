7c669fda3f2177ccd94e19505b3b56cab86ee6a2
RominaMalimban <romina.lyn@hotmail.it>
2023-02-09 10:54:46 +0100

    creazione colonne

--- database/migrations/2023_02_09_094337_create_projects_table.php
+++ database/migrations/2023_02_09_094337_create_projects_table.php
@@ -15,6 +15,13 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+
+            $table->string('name', 64)->unique();
+            $table->text('description')->nullable();
+            $table->string('main_image')->unique();
+            $table->date('release_date');
+            $table->string('repo_link')->unique();
+
             $table->timestamps();
         });
     }
