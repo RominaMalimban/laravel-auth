117a68a0336d897feabb603667912f25a41c37f6
RominaMalimban <romina.lyn@hotmail.it>
2023-02-09 12:10:38 +0100

    modifica commento errore

--- database/factories/ProjectFactory.php
+++ database/factories/ProjectFactory.php
@@ -5,7 +5,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Project>
  */
 class ProjectFactory extends Factory
 {
@@ -21,7 +21,7 @@ class ProjectFactory extends Factory
             'description'=> fake() -> boolean() 
                         ? fake() -> paragraph()
                         : '',
-            'main_image'=> fake()-> url(),
+            'main_image'=> fake()-> imageUrl(640, 480, 'animals', true),
             'release_date'=> fake()-> dateTime(),
             'repo_link'=> fake()-> url(),
         ];
