5391a539c0cf8c2550df15a159677bf3e055de08
RominaMalimban <romina.lyn@hotmail.it>
2023-02-11 23:22:25 +0100

    layout pagina loggata completato

--- resources/views/pages/logged.blade.php
+++ resources/views/pages/logged.blade.php
@@ -1,35 +1,62 @@
 @extends('layouts.main-layout')
 
 @section('content')
-    <div class="container ms-home">
-        <h1>Repositories</h1>
+    
+    {{-- titolo --}}
+    <h1 class="container myMainTitle">REPOSITORIES</h1>
+    
+    {{-- svg onda --}}
+    <svg class="myWave" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path  fill="#efd6b0" fill-opacity="1"                                                            
+d="M0,224L80,197.3C160,171,320,117,480,128C640,139,800,213,960,240C1120,267,1280,245,1360,234.7L1440,224L1440,320L1360,32           0C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z">
+        </path>
+    </svg>
 
-        <h3>
-            <a class="text-light" href="{{ route('projectCreate') }}">Create a new repository</a>
-        </h3>
-        
-        <div class="my-cards">
-            @foreach ($projects as $project)
-            <div class="card mb-4 p-3">
-                <a href="{{route('projectShow', $project)}}">
-                    <h2>Repository: {{$project -> name}}</h2>
-                </a>
-                <p class="my-description"> {{$project -> description 
-                                ? $project -> description 
-                                : ""}}
-                </p>
+    <div class="ms-wrapper">
+
+        <div class="container logged">
+
+            <h3 class="createForm">
+                <i class="fa-solid fa-plus"></i>
+                <a href="{{route('projectCreate')}}">Create a new repository</a>
+            </h3>  
+
+            <div class="myCards">
                 
-                <img src="{{ asset('storage/' . $project -> main_image) }}" alt="">
-               
-                <span>Release date: {{ $project -> release_date}}</span>
-                <span>Repo link: {{ $project -> repo_link}}</span>
+                @foreach ($projects as $project)
+                    <div class="card">
 
-                <a href="{{route('projectDelete', $project)}}">ELIMINA</a>
-                <a href="{{ route('projectEdit', $project) }}">EDIT</a>
-            </div> 
-        @endforeach
+                        <img src="{{ $project -> main_image}}" alt="">
+                        <img src="{{ asset('storage/' . $project -> main_image) }}" alt="">
+
+                        <div class="mytext">
+                            <a href="{{route('projectShow', $project)}}">
+                                <h3>{{$project -> name}}</h3>
+                            </a>
+                        </div>
+                        
+                        <p class="my-description"> 
+                            {{$project -> description 
+                              ? $project -> description 
+                              : ""}}
+                        </p>
+                        
+                        <span>Release date: {{ $project -> release_date}}</span>
+                        <div class="repoLink">
+                            <i class="fa-brands fa-github"></i>
+                            <a href="{{ $project -> repo_link}}">Link GitHub</a>
+                        </div>
+                        
+                        <div class="myButtons">
+                            <a href="{{route('projectDelete', $project)}}">Delete</a>
+                            <a class="myEdit" href="{{ route('projectEdit', $project) }}">Edit</a>
+                        </div>
+                        
+                    </div> 
+                @endforeach
+            </div>
         </div>
         
-        
     </div>
+ 
 @endsection
